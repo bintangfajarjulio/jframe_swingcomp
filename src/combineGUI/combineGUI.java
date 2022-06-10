ba2178be3b4a68d1fa7ceb4bb7a94fbcff9259a4
bintangfjulio <bintangin14@gmail.com>
2022-06-10 09:30:24 +0700

    initial commit

---
+++ src/combineGUI/combineGUI.java
@@ -0,0 +1,98 @@
+package combineGUI;
+
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.table.*;
+import javax.swing.tree.*;
+import java.util.*;
+
+public class combineGUI {
+	public static void main(String[] args) {
+		EventQueue.invokeLater(() -> {
+			JFrame frame = new PanelFrame();
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setVisible(true);
+			frame.setResizable(false);
+		});
+	}
+}
+
+class PanelFrame extends JFrame {
+	private DefaultTreeModel model;
+
+	private JTree tree;
+	private JList<String> list = new JList<>();
+	private JTable tabelData;
+
+	private String[] columnNames = { "Dummy" };
+	private Object[][] cells = { { null } };
+
+	DefaultListModel<String> modelList = new DefaultListModel<>();
+
+	private boolean removeNull = true;
+
+	public PanelFrame() {
+		setTitle("Combine Panel");
+
+		TableModel modelTable = new DefaultTableModel(cells, columnNames);
+
+		tabelData = new JTable(modelTable);
+		JScrollPane scrollPaneTable = new JScrollPane(tabelData);
+
+		DefaultMutableTreeNode dummy = new DefaultMutableTreeNode("Dummy");
+		TreeNode root = dummy;
+		model = new DefaultTreeModel(root);
+		tree = new JTree(model);
+		tree.setEditable(true);
+		JScrollPane scrollPaneTree = new JScrollPane(tree);
+
+		list.setModel(modelList);
+		JScrollPane scrollPaneList = new JScrollPane(list);
+
+		JPanel inputPanel = new JPanel();
+		inputPanel.setLayout(new FlowLayout());
+
+		JTextField textField = new JTextField(20);
+		inputPanel.add(textField);
+
+		JButton button = new JButton("Add");
+		inputPanel.add(button);
+		button.addActionListener(event -> textField.setText(""));
+		button.addActionListener(event -> {
+			DefaultMutableTreeNode inputRoot = (DefaultMutableTreeNode) model.getRoot();
+
+			DefaultMutableTreeNode newNode = new DefaultMutableTreeNode(textField.getText());
+			model.insertNodeInto(newNode, inputRoot, inputRoot.getChildCount());
+
+			TreeNode[] nodes = model.getPathToRoot(newNode);
+			TreePath path = new TreePath(nodes);
+			tree.scrollPathToVisible(path);
+		});
+
+		button.addActionListener(event -> {
+			modelList.addElement(textField.getText());
+		});
+
+		button.addActionListener(event -> {
+			DefaultTableModel dataModel = (DefaultTableModel) tabelData.getModel();
+			ArrayList<String> inputList = new ArrayList<>();
+			tabelData.setAutoCreateColumnsFromModel(true);
+			inputList.add(textField.getText());
+			dataModel.addRow(inputList.toArray());
+
+			if (removeNull) {
+				dataModel.removeRow(0);
+				removeNull = false;
+			}
+		});
+
+		JPanel contentPanel = new JPanel();
+		contentPanel.add(scrollPaneTree);
+		contentPanel.add(scrollPaneTable);
+		contentPanel.add(scrollPaneList);
+
+		add(contentPanel, BorderLayout.NORTH);
+		add(inputPanel, BorderLayout.SOUTH);
+		pack();
+	}
+}
